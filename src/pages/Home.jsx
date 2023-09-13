6675e91dcc10a7ff1df0aca2b802e2a3cbe6f47f
=ahmedyasser <=ahmedya3ser8@gmail.com>
2023-09-13 07:29:32 +0300

    Creating Testimonials and GetStarted

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import Features from "../components/Features";
+import GetStarted from "../components/GetStarted";
 import Landing from "../components/Landing";
 import StayProductive from "../components/StayProductive";
+import Testimonials from "../components/Testimonials";
 
 function Home() {
   return (
@@ -8,6 +10,8 @@ function Home() {
       <Landing />
       <Features />
       <StayProductive />
+      <Testimonials />
+      <GetStarted />
     </>
   )
 }
